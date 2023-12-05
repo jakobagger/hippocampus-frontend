5bfe814f996ee3a5c38d850967603c446d79ea9c
Lotte Eliasson <lottefacereliasson@hotmail.com>
2023-12-05 18:36:03 +0100

    Little style....

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import {
 import { initLogin } from "./pages/login/login.js";
 import { initDefaultMatrix } from "./pages/defaultMatrix/defaultMatrix.js";
 import { initAddMatrix } from "./pages/addMatrix/addMatrix.js";
-import { initAddSuits } from "./pages/addSuits/addSuits.js";
 import { initSignup } from "./pages/signup/signup.js";
 import { initQuiz } from "./pages/quiz/quiz.js";
 
@@ -19,7 +18,6 @@ window.addEventListener("load", async () => {
   const templateLogin = await loadHtml("./pages/login/login.html")
   const templateDefaultMatrix = await loadHtml("./pages/defaultMatrix/defaultMatrix.html")
   const templateAddMatrix = await loadHtml("./pages/addMatrix/addMatrix.html")
-  const templateAddSuits = await loadHtml("./pages/addSuits/addSuits.html")
   const templateSignup = await loadHtml("./pages/signup/signup.html")
   const templateQuiz = await loadHtml("./pages/quiz/quiz.html")
   const templateAdmin = await loadHtml("./pages/admin/admin.html")
@@ -65,10 +63,6 @@ window.addEventListener("load", async () => {
       "/addMatrix": () => {
         renderHtml(templateAddMatrix, "content")
         initAddMatrix()
-      },
-      "/addSuits": () => {
-        renderHtml(templateAddSuits, "content")
-        initAddSuits()
       },
         "/login": () => {
           renderHtml(templateLogin, "content");
