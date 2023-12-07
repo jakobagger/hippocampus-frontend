42107c1381b1bddd70568af1f1327a54e9467e88
Selmerr <molybdenum93@gmail.com>
2023-12-07 16:55:28 +0100

    fixed default matrix to get descriptions from values and suits

--- pages/defaultMatrix/defaultMatrix.js
+++ pages/defaultMatrix/defaultMatrix.js
@@ -14,7 +14,10 @@ async function getMatrixData(){
   try {
     const matrixFromServer = await fetch(URL).then(res => res.json())
     const matrixCards = matrixFromServer.cards
-    console.log(matrixCards)
+    const matrixSuits = matrixFromServer.suits
+    const matrixValues = matrixFromServer.values
+    renderMatrixValues(matrixValues)
+    renderMatrixSuits(matrixSuits)
     renderMatrixData(matrixCards)
     
   } catch (e){
@@ -22,11 +25,37 @@ async function getMatrixData(){
   }
 }
 
+function renderMatrixValues(data) {
+  const columnDivs = document.querySelectorAll("div[data-att]")
+  data.forEach(value => {
+      let valueName = value.valueName;
+
+      columnDivs.forEach(div => {
+        if(div.getAttribute("data-att").includes(valueName)) {
+          div.textContent = value.valueDescription
+        }
+      })
+
+      
+  })
+
+}
+
+function renderMatrixSuits(data) {
+
+  data.forEach(suit => {
+    let suitSelector = `.end-row[data-suit="${suit.suitName}"]`
+    let suitCell = document.querySelector(suitSelector)
+    suitCell.textContent = suit.suitDescription
+  })
+
+
+}
 
 function renderMatrixData(data) {
     data.forEach(card => {
-      let selector = `.grid-item[data-value="${card.value}"][data-suit="${card.suit}"]`
-      let cell = document.querySelector(selector)
+      let cellselector = `.grid-item[data-value="${card.value}"][data-suit="${card.suit}"]`
+      let cell = document.querySelector(cellselector)
       // Populate person
       let personDiv = cell.querySelector('.card-person');
       personDiv.textContent = card.person;
